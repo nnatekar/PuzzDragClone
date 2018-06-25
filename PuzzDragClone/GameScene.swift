81c83af7599212976c24ecf5cee8ac0fbfb224f7
Neil Natekar <neilnatekar@gmail.com>
2018-06-24 22:39:13 -0700

    touch added, having trouble with detecting contact

--- PuzzDragClone/GameScene.swift
+++ PuzzDragClone/GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 import GameplayKit
 
-enum Type : String {
+enum Type : String { // all different types of orbs
     case R = "ball-fire"
     case G = "ball-wood"
     case B = "ball-water"
@@ -19,25 +19,39 @@ enum Type : String {
     case other = "garbage"
 }
 
-class Orb {
+class Orb { // contains properties of each orb
     var Node = SKSpriteNode()
     var type = Type.other
+    var originalPos = CGPoint()
 }
 
-class GameScene: SKScene {
+func rowMajorConversion(column: Int, row: Int) -> Int{ // needed to convert 2D array to 1D
+    return ((6 * row) + (column))
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    private var orb = SKSpriteNode() // using for testing
+    private var orbs = [Orb]() // used to hold entire board of orbs
+    private var tileBackground : SKTileMapNode! // contains background of tiles
+    private var movingOrb = Orb()
     
-    private var orb = SKSpriteNode()
-    private var orbs = [[Orb]]()
-    private var tileBackground : SKTileMapNode!
-    var prevPt : CGPoint!
+    func didBegin(_ contact: SKPhysicsContact){
+        print("Starting collision")
+        let posA = contact.bodyA.node?.position
+        contact.bodyA.node?.run(SKAction.move(to: (contact.bodyB.node?.position)!, duration: 0))
+        contact.bodyB.node?.run(SKAction.move(to: posA!, duration:0))
+    }
     
     override func didMove(to view: SKView) {
         let tileBackground = childNode(withName: "BGTiles") as? SKTileMapNode
         self.tileBackground = tileBackground
-        for column in 0...5{
-            for row in 0...4{ // initialize all orbs in the board
+        self.physicsWorld.contactDelegate = self
+        
+        for row in 0...4{ // initialize all orbs in the board
+            for column in 0...5{
                 var text = SKTexture() // contains the orb texture
                 let currorb = Orb() // current orb to be initialized
+                
                 let rand = arc4random_uniform(6)
                 switch rand{ // choose color of orb based on random value
                 case 0:
@@ -62,25 +76,34 @@ class GameScene: SKScene {
                 
                 currorb.Node = SKSpriteNode(texture: text)
                 currorb.Node.position = tileBackground!.centerOfTile(atColumn: column, row: row)
-                print("\(tileBackground!.centerOfTile(atColumn: column, row: row))\n")
-                currorb.Node.setScale(1.15)
-                self.addChild(currorb.Node)
+                currorb.originalPos = currorb.Node.position
+                currorb.Node.setScale(1.21)
+                currorb.Node.physicsBody = SKPhysicsBody(circleOfRadius: 60.5)
+                currorb.Node.physicsBody?.categoryBitMask = 0x1 << 1
+                currorb.Node.physicsBody?.contactTestBitMask = 0x1 << 1
+                currorb.Node.physicsBody?.isDynamic = false
+                currorb.Node.physicsBody?.usesPreciseCollisionDetection = true
+                orbs.append(currorb)
+                self.addChild(orbs[rowMajorConversion(column: column, row: row)].Node)
             }
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        let touch = touches.first
+        let location = touch!.location(in: self)
+        for index in 0..<30{
+            if(nodes(at:location).contains(orbs[index].Node)){
+                movingOrb = orbs[index]
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch: AnyObject in touches {
             let location = touch.location(in: self)
-            if(nodes(at: location).contains(orb))
-            {
-                orb.run(SKAction.moveBy(x:location.x - orb.position.x, y:location.y - orb.position.y, duration: 0))
-                print("x: \(orb.position.x), y: \(orb.position.y)")
-            }
+            movingOrb.Node.run(SKAction.moveBy(x:location.x - movingOrb.Node.position.x, y:location.y - movingOrb.Node.position.y, duration: 0))
+            print("Moving: \(movingOrb.Node.position.x), \(movingOrb.Node.position.y), static: \(orbs[0].Node.position.x), \(orbs[0].Node.position.y)")
         }
         
     }

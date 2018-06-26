64c6bd33089868604e88002e5bf70b3b515277fb
Neil Natekar <neilnatekar@gmail.com>
2018-06-25 23:08:26 -0700

    Finally got orb movement working, need to create orb match detection algorithm

--- PuzzDragClone/GameScene.swift
+++ PuzzDragClone/GameScene.swift
@@ -43,21 +43,18 @@ func rowMajorConversion(column: Int, row: Int) -> Int{ // needed to convert 2D a
 
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    // TODO: Create orb clones and change contact categories
     
     private var orb = SKSpriteNode() // using for testing
     private var orbs = [Orb]() // used to hold entire board of orbs
     private var tileBackground : SKTileMapNode! // contains background of tiles
     private var movingClone = clonedOrb()
     private var movingOrb = Orb()
-    private var indToOrb = [Int : Int]() // dictionary containing translation of index to orb
     
     func didBegin(_ contact: SKPhysicsContact){
         // body A will be the moving orb
         // body b will be the orb to switch with
         let posA = movingClone.originalPos
-        let boardposA = rowMajorConversion(column: posA[1], row: posA[0])
-        let orbAindex = indToOrb[boardposA]
+        let indexA = rowMajorConversion(column: posA[1], row: posA[0])
         var bodyb = SKNode()
         
         // determine which contact body is body B
@@ -71,31 +68,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // determine what row and column body B is in, determine it's index on board
         let rowB = tileBackground.tileRowIndex(fromPosition: bodyb.position)
         let colB = tileBackground.tileColumnIndex(fromPosition: bodyb.position)
-        let boardposB = rowMajorConversion(column: colB, row: rowB)
-        let orbBindex = indToOrb[boardposB]
+        let indexB = rowMajorConversion(column: colB, row: rowB)
         
-        print("\(orbs[orbBindex!].type): [\(orbs[orbBindex!].originalPos[0])][\(orbs[orbBindex!].originalPos[1])] to: [\(posA[0])][\(posA[1])]")
-        // change body B's original position, move it, and update dictionary value at the orb
-        if(orbs[orbBindex!].originalPos == movingOrb.originalPos){
-            print("Moving original! Other orb = \(bodyb)")
-        }
+        // change body B's original position, move it
+        movingOrb.Node.run(SKAction.move(to: orbs[indexB].Node.position, duration: 0))
+        orbs[indexB].originalPos = posA
+        orbs[indexB].Node.run(SKAction.move(to: tileBackground.centerOfTile(atColumn: posA[1], row: posA[0]), duration: 0.25))
         
-        movingOrb.Node.run(SKAction.move(to: orbs[orbBindex!].Node.position, duration: 0))
-        orbs[orbBindex!].originalPos = posA
-        orbs[orbBindex!].Node.run(SKAction.move(to: tileBackground.centerOfTile(atColumn: posA[1], row: posA[0]), duration: 0))
-        indToOrb[boardposB] = orbAindex
+        orbs[indexA] = orbs[indexB]
+        orbs[indexB] = movingOrb
         
-        // change body A's original position and update its dictionary value
+        // change body A's original position
         movingClone.originalPos = [rowB, colB]
-        indToOrb[boardposA] = orbAindex
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("Final = \(movingClone.type): [\(movingOrb.originalPos[0])][\(movingOrb.originalPos[1])] to [\(movingClone.originalPos[0])][\(movingClone.originalPos[1])]")
         movingOrb.originalPos = movingClone.originalPos
         movingOrb.Node.run(SKAction.move(to: tileBackground.centerOfTile(atColumn: movingOrb.originalPos[1], row: movingOrb.originalPos[0]), duration: 0))
         movingOrb.Node.physicsBody?.categoryBitMask = 1
+        movingOrb.Node.texture = SKTexture(imageNamed: movingOrb.type.rawValue)
         movingClone.Node.removeFromParent()
+        movingOrb.Node.removeFromParent()
+        self.addChild(movingOrb.Node)
     }
     
     override func didMove(to view: SKView) {
@@ -107,8 +101,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             for column in 0...5{
                 var text = SKTexture() // contains the orb texture
                 let currorb = Orb() // current orb to be initialized
-                
-                indToOrb[rowMajorConversion(column: column, row: row)] = rowMajorConversion(column: column, row: row)
                 
                 let rand = arc4random_uniform(6)
                 switch rand{ // choose color of orb based on random value
@@ -153,14 +145,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let col = tileBackground.tileColumnIndex(fromPosition: location)
         
         // set up orb to be moved, temporarily change categorybitmask so not affected by contact
-        movingOrb = orbs[indToOrb[rowMajorConversion(column: col, row: row)]!]
+        movingOrb = orbs[rowMajorConversion(column: col, row: row)]
         movingOrb.Node.physicsBody?.categoryBitMask = 0
         movingOrb.Node.physicsBody?.collisionBitMask = 0
         
         // set up orb clone which will be moved
         movingClone = clonedOrb(originalOrb: movingOrb)
         movingClone.originalOrbIndex = rowMajorConversion(column: col, row: row)
-        movingClone.Node = SKSpriteNode(texture: SKTexture(imageNamed: movingClone.type.rawValue + "-clone"))
+        movingClone.Node = SKSpriteNode(texture: SKTexture(imageNamed: movingClone.type.rawValue))
         movingClone.Node.position = movingOrb.Node.position
         movingClone.Node.setScale(1.21)
         movingClone.Node.physicsBody = SKPhysicsBody(circleOfRadius: 10)
@@ -168,6 +160,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         movingClone.Node.physicsBody?.contactTestBitMask = 1
         movingClone.Node.physicsBody?.affectedByGravity = false
         self.addChild(movingClone.Node)
+        
+        movingOrb.Node.texture = SKTexture(imageNamed: movingOrb.type.rawValue + "-clone")
+        movingOrb.Node.removeFromParent()
+        self.addChild(orbs[rowMajorConversion(column: col, row: row)].Node)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {

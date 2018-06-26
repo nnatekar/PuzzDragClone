aae17688621c4d038df26ea2f8515579f9b9cac5
Neil Natekar <neilnatekar@gmail.com>
2018-06-26 14:50:14 -0700

    Most combos are working, need to work on irregular shapes

--- PuzzDragClone/GameScene.swift
+++ PuzzDragClone/GameScene.swift
@@ -50,6 +50,87 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var movingClone = clonedOrb()
     private var movingOrb = Orb()
     
+    func findMatches() -> [[Int]] {
+        var all_matches = [[Int]]()
+        
+        // find all vertical matches
+        var vertical_matches = [[Int]]()
+        var match = [Int]()
+        var colsChecked = [Int]()
+        
+        for row in 0...2{ // ORBS GO BOTTOM -> TOP NOT TOP -> BOTTOM
+            for col in 0...5{
+                if(vertical_matches.count > 0 && colsChecked.contains(col)) { // continue if column has already been fully checked
+                    print("Continued to next!")
+                    continue
+                }
+                let index = rowMajorConversion(column: col, row: row)
+                let orb1 = orbs[index].type
+                let orb2 = orbs[index+6].type
+                let orb3 = orbs[index+12].type
+                
+                if(orb1 == orb2 && orb2 == orb3) { // all orbs are same type
+                    match = [index, index+6, index+12]
+                    colsChecked.append(col)
+                    var checkMore = 5 - row
+                    var currMatchedOrbs = 3
+                    
+                    while(checkMore > 3){ // continue checking the entire column so it can be skipped next iteration
+                        let nextOrb = orbs[index + currMatchedOrbs * 6].type
+                        if(nextOrb == orb1){
+                            match.append(index+currMatchedOrbs * 6)
+                            currMatchedOrbs += 1
+                        }
+                        checkMore -= 1
+                    }
+                    
+                    vertical_matches.append(match)
+                }
+            }
+        }
+        
+        // find horizontal matches
+        var horizontal_matches = [[Int]]()
+        match = [Int]()
+        var rowsChecked = [Int]()
+        
+        for col in 0...3{
+            for row in 0...4{
+                if(horizontal_matches.count > 0 && colsChecked.contains(col)){
+                    continue
+                }
+                let index = rowMajorConversion(column: col, row: row)
+                let orb1 = orbs[index].type
+                let orb2 = orbs[index+1].type
+                let orb3 = orbs[index+2].type
+                
+                print("\(orbs[index].Node)\n\(orbs[index+1].Node)\n\(orbs[index+2].Node)\n\n")
+                if(orb1 == orb2 && orb2 == orb3){
+                    match = [index, index + 1, index + 2]
+                    rowsChecked.append(row)
+                    var checkMore = 6 - col
+                    var currMatchedOrbs = 3
+                    
+                    while(checkMore > 3){
+                        let nextOrb = orbs[index + currMatchedOrbs].type
+                        if(nextOrb == orb1){
+                            match.append(index + currMatchedOrbs)
+                            currMatchedOrbs += 1
+                        }
+                        checkMore -= 1
+                    }
+                    horizontal_matches.append(match)
+                }
+            }
+        }
+        
+        // find intersections between horizontal and vertical
+        
+        all_matches += vertical_matches // testing
+        all_matches += horizontal_matches
+        return all_matches
+    }
+    
     func didBegin(_ contact: SKPhysicsContact){
         // body A will be the moving orb
         // body b will be the orb to switch with
@@ -71,7 +152,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let indexB = rowMajorConversion(column: colB, row: rowB)
         
         // change body B's original position, move it
-        movingOrb.Node.run(SKAction.move(to: orbs[indexB].Node.position, duration: 0))
+        movingOrb.Node.run(SKAction.move(to: tileBackground.centerOfTile(atColumn: colB, row: rowB), duration: 0))
         orbs[indexB].originalPos = posA
         orbs[indexB].Node.run(SKAction.move(to: tileBackground.centerOfTile(atColumn: posA[1], row: posA[0]), duration: 0.25))
         
@@ -90,6 +171,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         movingClone.Node.removeFromParent()
         movingOrb.Node.removeFromParent()
         self.addChild(movingOrb.Node)
+     //   movingOrb.Node.run(SKAction.fadeOut(withDuration: 0.5))
+        
+        let matchedSet = findMatches()
+        var comboNum = 1
+        for i in 0..<matchedSet.count{
+            let label = SKLabelNode(fontNamed: "DIN Condensed")
+            label.text = "Combo" + String(comboNum)
+            label.fontSize = 31
+            comboNum += 1
+            for j in 0..<matchedSet[i].count{
+                self.orbs[matchedSet[i][j]].Node.run(SKAction.fadeOut(withDuration: 0.5))
+                label.position = CGPoint(x: orbs[matchedSet[i][j]].Node.position.x, y: orbs[matchedSet[i][j]].Node.position.y)
+                
+            }
+            self.addChild(label)
+        }
     }
     
     override func didMove(to view: SKView) {
@@ -128,7 +225,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 currorb.Node.position = tileBackground!.centerOfTile(atColumn: column, row: row)
                 currorb.originalPos = [row, column]
                 currorb.Node.setScale(1.21)
-                currorb.Node.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+                currorb.Node.physicsBody = SKPhysicsBody(circleOfRadius: 25)
                 currorb.Node.physicsBody?.categoryBitMask = 1
                 currorb.Node.physicsBody?.affectedByGravity = false
                 orbs.append(currorb)

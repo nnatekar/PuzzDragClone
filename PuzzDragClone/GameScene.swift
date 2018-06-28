30242c4a5ae888ea3034616c9c6d456453b5607c
Neil Natekar <neilnatekar@gmail.com>
2018-06-28 14:08:49 -0700

    Cleaned up code, got skyfall half working, need to work on other half + finish fixing combo checking

--- PuzzDragClone/GameScene.swift
+++ PuzzDragClone/GameScene.swift
@@ -9,7 +9,9 @@
 import SpriteKit
 import GameplayKit
 
-enum Type : String { // all different types of orbs
+
+// all different types of orbs
+enum Type : String {
     case R = "ball-fire"
     case G = "ball-wood"
     case B = "ball-water"
@@ -19,27 +21,27 @@ enum Type : String { // all different types of orbs
     case other = "garbage"
 }
 
-class Orb { // contains properties of each orb
+// Class that contains information about orbs
+// Node = SKSpriteNode the orb is related to
+// type = the type of the orb (ie. fire, water, wood)
+// originalPos = the row and column of the board the orb is in
+class Orb {
     var Node = SKSpriteNode()
     var type = Type.other
-    var originalPos = [Int]()
-}
-
-class clonedOrb: Orb {
-    var originalOrbIndex = Int()
+    var originalPos = [Int]() // [row, column]
     
-    override init(){super.init()}
+    init(){}
     init(originalOrb: Orb){
-        super.init()
         self.type = originalOrb.type
         self.originalPos = originalOrb.originalPos
     }
 }
 
-func rowMajorConversion(column: Int, row: Int) -> Int{ // needed to convert 2D array to 1D
+
+// needed to convert 2D array to 1D
+func rowMajorConversion(column: Int, row: Int) -> Int{
     return ((6 * row) + (column))
 }
-
 
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
@@ -47,13 +49,121 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var orb = SKSpriteNode() // using for testing
     private var orbs = [Orb]() // used to hold entire board of orbs
     private var tileBackground : SKTileMapNode! // contains background of tiles
-    private var movingClone = clonedOrb()
+    private var movingClone = Orb()
     private var movingOrb = Orb()
     
-    func skyfall(ind: [Int]){
-        // orbs above garbage first fall down, then all blanks get replaced with new
+    /**
+        Function to create new orbs
+        @param pos Position at which orb needs to be created
+        @return currorb Newly created orb
+     */
+    func orbCreate(pos: CGPoint) -> Orb{
+        let currorb = Orb()
+        var text = SKTexture()
+        let rand = arc4random_uniform(6)
+        switch rand{ // choose color of orb based on random value
+        case 0:
+            currorb.type = Type.R
+            text = SKTexture(imageNamed: currorb.type.rawValue)
+        case 1:
+            currorb.type = Type.G
+            text = SKTexture(imageNamed: currorb.type.rawValue)
+        case 2:
+            currorb.type = Type.B
+            text = SKTexture(imageNamed: currorb.type.rawValue)
+        case 3:
+            currorb.type = Type.L
+            text = SKTexture(imageNamed: currorb.type.rawValue)
+        case 4:
+            currorb.type = Type.D
+            text = SKTexture(imageNamed: currorb.type.rawValue)
+        default:
+            currorb.type = Type.H
+            text = SKTexture(imageNamed: currorb.type.rawValue)
+        }
+        
+        currorb.Node = SKSpriteNode(texture: text)
+        currorb.Node.position = pos
+        currorb.originalPos = [tileBackground.tileRowIndex(fromPosition: pos), tileBackground.tileColumnIndex(fromPosition: pos)]
+        currorb.Node.setScale(1.21)
+        currorb.Node.physicsBody = SKPhysicsBody(circleOfRadius: 40)
+        currorb.Node.physicsBody?.categoryBitMask = 1
+        currorb.Node.physicsBody?.affectedByGravity = false
+        return currorb
     }
     
+    /**
+        Function to make orbs fall down and replace matched orbs
+        @param ind Indices of orbs that need to be replaced
+     */
+    func skyfall(ind: [Int]){
+        // orbs above garbage first fall down
+        var skyfallOne = [Int]() // indices of all orbs that need to fall only one position down
+        var skyfallTwo = [Int]() // indices that need to fall two down
+        var skyfallThree = [Int]() // need to fall 3 down
+        var skyfallFour = [Int]()
+        for i in (6...29).reversed(){ // orbs 0-5 never need to skyfall
+            if(orbs[i].type == Type.other){ // current is garbage, skip
+                continue
+            }
+            var x = i - 6
+            var count = 0
+            while(x >= 0){ // count how many positions to skyfall
+                if(orbs[x].type == Type.other){
+                    count += 1
+                }
+                x -= 6
+            }
+            
+            switch count{
+            case 1:
+                skyfallOne.append(i)
+            case 2:
+                skyfallTwo.append(i)
+            case 3:
+                skyfallThree.append(i)
+            case 4:
+                skyfallFour.append(i)
+            default:
+                print("")
+            }
+        }
+        
+        // move down all orbs to replace garbage
+        for i in skyfallOne.reversed(){
+            orbs[i-6] = orbs[i]
+            orbs[i].type = Type.other
+            orbs[i-6].originalPos = [orbs[i].originalPos[0] - 1, orbs[i].originalPos[1]] // go down one row
+            orbs[i-6].Node.run(SKAction.move(by: CGVector(dx: 0, dy: -125), duration: 0.5)) // move it down
+        }
+        for i in skyfallTwo.reversed(){
+            orbs[i-12] = orbs[i]
+            orbs[i].type = Type.other
+            orbs[i-12].originalPos = [orbs[i].originalPos[0] - 2, orbs[i].originalPos[1]] // go down one row
+            orbs[i-12].Node.run(SKAction.move(by: CGVector(dx: 0, dy: -125 * 2), duration: 0.5)) // move it down
+        }
+        for i in skyfallThree.reversed(){
+            orbs[i-18] = orbs[i]
+            orbs[i].type = Type.other
+            orbs[i-18].originalPos = [orbs[i].originalPos[0] - 3, orbs[i].originalPos[1]] // go down one row
+            orbs[i-18].Node.run(SKAction.move(by: CGVector(dx: 0, dy: -125 * 3), duration: 0.5)) // move it down
+        }
+        for i in skyfallFour.reversed(){
+            orbs[i-24] = orbs[i]
+            orbs[i].type = Type.other
+            orbs[i-24].originalPos = [orbs[i].originalPos[0] - 4, orbs[i].originalPos[1]] // go down one row
+            orbs[i-24].Node.run(SKAction.move(by: CGVector(dx: 0, dy: -125 * 4), duration: 0.5)) // move it down
+        }
+        
+        
+        // next, fill in blank space at the top with new orbs
+        
+    }
+    
+    /**
+        Function to determine which orbs in the board are matched
+        @return all_matches Indices of all orbs which have been matched
+     */
     func findMatches() -> [[Int]] {
         var all_matches = [[Int]]()
         
@@ -62,10 +172,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         var match = [Int]()
         var colsChecked = [Int]()
         
-        for row in 0...2{ // ORBS GO BOTTOM -> TOP NOT TOP -> BOTTOM
+        for row in 0...2{
             for col in 0...5{
-                if(vertical_matches.count > 0 && colsChecked.contains(col)) { // continue if column has already been fully checked
-                    print("Continued to next!")
+                
+                // continue if column has already been fully checked
+                if(vertical_matches.count > 0 && colsChecked.contains(col)) {
                     continue
                 }
                 let index = rowMajorConversion(column: col, row: row)
@@ -73,18 +184,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 let orb2 = orbs[index+6].type
                 let orb3 = orbs[index+12].type
                 
-                if(orb1 == orb2 && orb2 == orb3) { // all orbs are same type
+                // all orbs are same type
+                if(orb1 == orb2 && orb2 == orb3) {
                     match = [index, index+6, index+12]
                     colsChecked.append(col)
                     var checkMore = 5 - row
                     var currMatchedOrbs = 3
                     
-                    while(checkMore > 3){ // continue checking the entire column so it can be skipped next iteration
+                    // continue checking the entire column so it can be skipped next iteration
+                    while(checkMore > 3){
                         let nextOrb = orbs[index + currMatchedOrbs * 6].type
+                        
+                        // next orb in column is part of current match
                         if(nextOrb == orb1){
                             match.append(index+currMatchedOrbs * 6)
                             currMatchedOrbs += 1
                         }
+                        
                         checkMore -= 1
                     }
                     
@@ -100,6 +216,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         for col in 0...3{
             for row in 0...4{
+                
                 if(horizontal_matches.count > 0 && colsChecked.contains(col)){
                     continue
                 }
@@ -108,7 +225,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 let orb2 = orbs[index+1].type
                 let orb3 = orbs[index+2].type
                 
-                print("\(orbs[index].Node)\n\(orbs[index+1].Node)\n\(orbs[index+2].Node)\n\n")
                 if(orb1 == orb2 && orb2 == orb3){
                     match = [index, index + 1, index + 2]
                     rowsChecked.append(row)
@@ -121,8 +237,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                             match.append(index + currMatchedOrbs)
                             currMatchedOrbs += 1
                         }
+                        
                         checkMore -= 1
                     }
+                    
                     horizontal_matches.append(match)
                 }
             }
@@ -135,6 +253,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         while(i < vertical_matches.count){
             while(j < horizontal_matches.count){
                 let x = vertical_matches[i].filter(horizontal_matches[j].contains) // intersection between both
+                
+                // if there is an intersection, remove it from vertical & horizontal so it's not counted extra times
                 if(x.count > 0){
                     var singleMatch = vertical_matches[i] + horizontal_matches[j]
                     vertical_matches.remove(at: i)
@@ -150,6 +270,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return all_matches
     }
     
+    
+    /**
+        Function which is called as soon as a contact begins
+        Switches the two orbs that made contact
+     */
     func didBegin(_ contact: SKPhysicsContact){
         // body A will be the moving orb
         // body b will be the orb to switch with
@@ -175,6 +300,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         orbs[indexB].originalPos = posA
         orbs[indexB].Node.run(SKAction.move(to: tileBackground.centerOfTile(atColumn: posA[1], row: posA[0]), duration: 0.25))
         
+        // switch orbs around in main array
         orbs[indexA] = orbs[indexB]
         orbs[indexB] = movingOrb
         
@@ -182,15 +308,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         movingClone.originalPos = [rowB, colB]
     }
     
+    
+    /**
+        Function that is called when touches end
+        Detects matches and starts skyfalls
+     */
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         movingOrb.originalPos = movingClone.originalPos
         movingOrb.Node.run(SKAction.move(to: tileBackground.centerOfTile(atColumn: movingOrb.originalPos[1], row: movingOrb.originalPos[0]), duration: 0))
         movingOrb.Node.physicsBody?.categoryBitMask = 1
         movingOrb.Node.texture = SKTexture(imageNamed: movingOrb.type.rawValue)
         movingClone.Node.removeFromParent()
-        movingOrb.Node.removeFromParent()
-        self.addChild(movingOrb.Node)
-     //   movingOrb.Node.run(SKAction.fadeOut(withDuration: 0.5))
         
         let matchedSet = findMatches()
         var comboNum = 1
@@ -207,13 +335,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 orbs[matchedSet[i][j]].Node.run(SKAction.fadeOut(withDuration: 0.5))
                 label.position = CGPoint(x: orbs[matchedSet[i][j]].Node.position.x, y: orbs[matchedSet[i][j]].Node.position.y)
                 orbs[matchedSet[i][j]].type = Type.other // know that the orb is finished
+                orbs[matchedSet[i][j]].Node.removeFromParent()
             }
             self.addChild(label)
         }
         
-        skyfall(ind: finishedIndices)
+        // delay skyfall by 2 seconds so user can see the number of combos they made
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.skyfall(ind: finishedIndices)
+        }
     }
     
+    
+    /**
+        Function that intializes game and board
+     */
     override func didMove(to view: SKView) {
         let tileBackground = childNode(withName: "BGTiles") as? SKTileMapNode
         self.tileBackground = tileBackground
@@ -221,44 +357,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         for row in 0...4{ // initialize all orbs in the board
             for column in 0...5{
-                var text = SKTexture() // contains the orb texture
-                let currorb = Orb() // current orb to be initialized
-                
-                let rand = arc4random_uniform(6)
-                switch rand{ // choose color of orb based on random value
-                case 0:
-                    currorb.type = Type.R
-                    text = SKTexture(imageNamed: currorb.type.rawValue)
-                case 1:
-                    currorb.type = Type.G
-                    text = SKTexture(imageNamed: currorb.type.rawValue)
-                case 2:
-                    currorb.type = Type.B
-                    text = SKTexture(imageNamed: currorb.type.rawValue)
-                case 3:
-                    currorb.type = Type.L
-                    text = SKTexture(imageNamed: currorb.type.rawValue)
-                case 4:
-                    currorb.type = Type.D
-                    text = SKTexture(imageNamed: currorb.type.rawValue)
-                default:
-                    currorb.type = Type.H
-                    text = SKTexture(imageNamed: currorb.type.rawValue)
-                }
-                
-                currorb.Node = SKSpriteNode(texture: text)
-                currorb.Node.position = tileBackground!.centerOfTile(atColumn: column, row: row)
-                currorb.originalPos = [row, column]
-                currorb.Node.setScale(1.21)
-                currorb.Node.physicsBody = SKPhysicsBody(circleOfRadius: 40)
-                currorb.Node.physicsBody?.categoryBitMask = 1
-                currorb.Node.physicsBody?.affectedByGravity = false
-                orbs.append(currorb)
+                orbs.append(orbCreate(pos: tileBackground!.centerOfTile(atColumn: column, row: row)))
                 self.addChild(orbs[rowMajorConversion(column: column, row: row)].Node)
             }
         }
     }
-    
+
+
+    /**
+        Function that is called when touches begin
+        Creates an orb clone used to move around other orbs
+     */
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
         let location = touch!.location(in: self)
@@ -272,8 +381,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         movingOrb.Node.physicsBody?.collisionBitMask = 0
         
         // set up orb clone which will be moved
-        movingClone = clonedOrb(originalOrb: movingOrb)
-        movingClone.originalOrbIndex = rowMajorConversion(column: col, row: row)
+        movingClone = Orb(originalOrb: movingOrb)
         movingClone.Node = SKSpriteNode(texture: SKTexture(imageNamed: movingClone.type.rawValue))
         movingClone.Node.position = movingOrb.Node.position
         movingClone.Node.setScale(1.21)
@@ -288,6 +396,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(orbs[rowMajorConversion(column: col, row: row)].Node)
     }
     
+    
+    /**
+        Function that is called whenever user moves orb
+        Moves orb along with user's finger
+     */
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         // only use first touch every time it's moved so it runs faster
         let touch = touches.first
